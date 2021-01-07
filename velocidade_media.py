3fb886d8eceb35879dcb8f1c4093892173cf6175
mtfelipe <67012469+mtfelipe@users.noreply.github.com>
2021-01-07 04:57:59 -0300

    Add files via upload

---
+++ velocidade_media.py
@@ -0,0 +1,8 @@
+#Calculadora de Velocidade média
+
+distancia = float(input('Qual foi a distancia percorrida em quilometros? '))
+tempo = float(input('Quantas horas se passaram?) '))
+
+vm = distancia / tempo
+
+print('A velocidade média é de', vm, 'km/h!')
